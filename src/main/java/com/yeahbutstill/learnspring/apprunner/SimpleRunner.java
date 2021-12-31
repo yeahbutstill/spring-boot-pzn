616cc46ef1e2d2e966f3b07479e9e805d3074fc1
Dani Setiawan <mayadani28@gmail.com>
2021-12-31 13:05:00 +0700

    Application Runner

---
+++ src/main/java/com/yeahbutstill/learnspring/apprunner/SimpleRunner.java
@@ -0,0 +1,18 @@
+package com.yeahbutstill.learnspring.apprunner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class SimpleRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<String> profiles = args.getOptionValues("profiles");
+        SimpleRunner.log.info("Profiles {}", profiles);
+    }
+}
